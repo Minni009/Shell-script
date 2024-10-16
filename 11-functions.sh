0352a939ea28a56326116f64346b8af4cfd16b13
Kavitha <kavithakavi2000reddy@gmail.com>
2024-10-16 21:52:11 +0530

    Learning if conditions in Shell

---
+++ 11-functions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ID=$(id -u)
+
+VALIDATE() {
+    if [#! -ne 0]
+    then 
+      echo "Error: installing MYSQL is failed"
+    else
+      echo "MYSQL Installation completed"
+    fi
+}
+if [ $ID -ne 0 ]
+then 
+  echo "Error: Please run this script in root access"
+  exit 1
+else
+  echo "Your are root user"
+fi
+yum install mysql -y
+VALIDATE "Installing MYSQL"
+yum install git -y
+ALIDATE "Installing GIT"
+
+
