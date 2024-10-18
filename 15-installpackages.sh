defcf54146dbb0d4d90a1dd58600c87a4efef256
Kavitha <kavithakavi2000reddy@gmail.com>
2024-10-18 09:11:01 +0530

    Installed packages using if and loops

--- 15-installpackages.sh
+++ 15-installpackages.sh
@@ -6,7 +6,7 @@ Y="\e[33m"
 N="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="\tmp\$0-$TIMESTAMP.log
-echo "Script started executing at $TIMESTAMP &>> LOGFILE
+echo "Script started executing at $TIMESTAMP &>> $LOGFILE
 
 VALIDATE() {
     if [ $1 -ne 0 ]
