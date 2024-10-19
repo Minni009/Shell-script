876ab9f265babdb6a18a529023648f84c321d8d1
Kavitha <kavithakavi2000reddy@gmail.com>
2024-10-19 21:55:00 +0530

    Deleting logs

---
+++ IFS.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+file=/etc/passwd
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -f $file]
+then
+   echo -e " $R source directory: $SOURCE_DIR does not exists. $N"
+fi
+
+while IFS=":" read -r username password userid groupid userfullname home_directory Shell_Path
+do
+  echo "username: $username"
+  echo "userid: $userid"
+  echo "groupid: $groupid"
+done < $file
