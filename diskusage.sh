41464f97f304fa62a0e89ef713ffb442254f6ec0
Kavitha <kavithakavi2000reddy@gmail.com>
2024-10-19 22:53:28 +0530

    Deleting logs

---
+++ diskusage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
+DISK_THERSHOLD=1
+
+while IFS= read line
+do 
+  usage=$(echo $line | awk '{print $6F}'| cut -d % -f1)
+  partition=$(echo $line | awk '{ print $1F}')
+  if [ $usage -ge $DISK_THERSHOLD ]
+  then
+   message+="High Disk Usage on $partition: $usage\n"
+   fi
+done <<< $DISK_USAG
+
+echo -e "MESSAGE: $message"
