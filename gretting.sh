aaaae9b87781be46e5c3a926cc27618ed553df6e
Kavitha <kavithakavi2000reddy@gmail.com>
2024-10-20 18:05:51 +0530

    Deleting logs

--- gretting.sh
+++ gretting.sh
@@ -21,3 +21,12 @@ while getopts ":n:w:h" opt; do
           
     esac
 done
+
+if [ -z "$NAME"] || [ -z $WISHES ];
+then
+  echo "ERROR: Both -n and -w are mandatory options."
+  USAGE
+  exit 1
+fi
+
+echo "Hello $NAME. $WISHES. I have been learning shell script"
