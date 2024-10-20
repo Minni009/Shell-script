4bfc4f0504ab116c9462669c8355d86e14448d03
Kavitha <kavithakavi2000reddy@gmail.com>
2024-10-20 17:54:59 +0530

    Deleting logs

---
+++ gretting.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+NAME=""
+WISHES=""
+
+USAGE(){
+    echo "USAGE:: $(basename $0) -n <name> -w <wishes>"
+    echo "Options::"
+    echo " -n, specify the name (mandatory) "
+    echo "-w specify the wishes. ex, Good Morning"
+    echo "-h Dispaly Help and exit " 
+}
+
+while getopts ":n:w:h" opt; do
+     case $opt in
+          n) NAME="$OPTARG";;
+          w) WHISHES="$OPTARG";;
+          h|*) USAGE; exit;;
+          
+    esac
+done
